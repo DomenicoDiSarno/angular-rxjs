30f30d27ce14dc99074598a44cb6134c2a3a284c
Domenico Di Sarno <domenico.disarno@builders-iot.com>
2020-02-18 22:19:38 +0100

    Update project

--- src/app/cdk.module.ts
+++ src/app/cdk.module.ts
@@ -5,7 +5,7 @@ import {ObserversModule} from '@angular/cdk/observers';
 import {OverlayModule} from '@angular/cdk/overlay';
 import {PlatformModule} from '@angular/cdk/platform';
 import {PortalModule} from '@angular/cdk/portal';
-import {ScrollDispatchModule} from '@angular/cdk/scrolling';
+import {ScrollingModule as ScrollDispatchModule} from '@angular/cdk/scrolling';
 import {CdkStepperModule} from '@angular/cdk/stepper';
 import {CdkTableModule} from '@angular/cdk/table';
 
